9336b17f5278fce0c7d98b770ea16faf280ce56f
kska0221 <aksinia.ra@gmail.com>
2025-05-07 23:30:46 +0500

    Homework 2

--- src/main/java/org/example/Task1.java
+++ src/main/java/org/example/Task1.java
@@ -4,16 +4,12 @@ public class Task1 {
     public static void main(String[] args) {
         String cardNumber1 = "1234 5678 9012 3456";
 
-        for (char i = '0'; i <= '9'; i++) {
-            cardNumber1 = cardNumber1.replace(i, '*');
-        }
-        System.out.println("Карта 1: " + cardNumber1);
+        String subString1 = cardNumber1.replace(" ", "").substring(12,16);
+        System.out.println("Карта 1: **** **** **** " + subString1);
 
 
-        String cardNumber2 = "1234567890123456";
-        for (char i = '0'; i <= '9'; i++) {
-            cardNumber2 = cardNumber2.replace(i, '*');
-        }
-        System.out.println("Карта 2: " + cardNumber2);
+        String cardNumber2 = "1234567890129999";
+        String subString2 = cardNumber2.replace(" ", "").substring(12,16);
+        System.out.println("Карта 2: **** **** **** " + subString2);
     }
 }
